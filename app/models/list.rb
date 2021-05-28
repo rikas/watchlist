f3b611238b419c72d670eab8900fdc4ba4d27058
rikas <oterosantos@gmail.com>
2021-05-28 11:03:24 +0100

    Add cover uploads

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,4 +3,6 @@ class List < ApplicationRecord
   has_many :movies, through: :bookmarks
 
   validates :name, presence: true, uniqueness: { message: 'has been used, you fool!' }
+
+  has_one_attached :cover
 end
